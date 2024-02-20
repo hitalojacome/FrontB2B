83eddd20f9f00d3091f89a97c7ba1e1d2dd3d599
HITALO JACOME <jacomehitalo@gmail.com>
2024-02-20 13:35:50 -0300

    bug botao

--- js/home.js
+++ js/home.js
@@ -1,6 +1,10 @@
 $(".menu_item").click(function () {
     var $currentSubMenu = $(this).find(".menu_options");
     $(".menu_options").not($currentSubMenu).slideUp();
+    $(".menu_item").not($(this)).removeClass("active");
+    $(".menu_vector").not($(this).find(".menu_vector")).css("transform", "none");
     $currentSubMenu.slideToggle();
     $(this).toggleClass("active");
+ 
+    $(this).find(".menu_vector").css("transform", "");
 });
