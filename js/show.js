2e97705521b9a0134b582a74919d5810e7dad600
Hitalo Jacome <jacomehitalo@gmail.com>
2024-03-21 12:55:12 -0300

    alterações

--- js/show.js
+++ js/show.js
@@ -1,8 +1,9 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var out = document.getElementById("sair");
 
 // Get the button that opens the modal
-var btn = document.getElementById("botao_novoUsuario");
+var btn = document.getElementById("botao_novo");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -11,7 +12,7 @@ btn.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal || event.target == out) {
     modal.style.display = "none";
   }
 }
